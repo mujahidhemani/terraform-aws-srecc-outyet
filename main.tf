e65a2ce05d789e3f809716e3795c9ab4c2648085
Mujahid Hemani <9062439+mujahidhemani@users.noreply.github.com>
2019-11-02 21:58:02 +0000

    update the vpc module version, add the ec2-autoscale module

--- main.tf
+++ main.tf
@@ -4,8 +4,14 @@ provider "aws" {
 
 module "srecc-vpc" {
   source  = "mujahidhemani/srecc-vpc/aws"
-  version = "1.0.0"
+  version = "1.0.1"
 }
 
 
+module "srecc-ec2-autoscale" {
+  source  = "mujahidhemani/srecc-ec2-autoscale/aws"
+  version = "1.0.0"
+  vpc_id = "${module.srecc-vpc.vpc_id}"
+  subnet_ids = "${module.srecc-vpc.all_subnet_ids}"
+}
 
