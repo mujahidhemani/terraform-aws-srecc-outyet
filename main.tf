2df8b8097dda9089ccc6a1a0078b823ba2a80b3e
Mujahid Hemani <9062439+mujahidhemani@users.noreply.github.com>
2019-11-03 20:20:44 +0000

    add the load-balancer module, bump version of the autoscale module and include the new required target_group_arn module

--- main.tf
+++ main.tf
@@ -8,10 +8,19 @@ module "srecc-vpc" {
 }
 
 
-module "srecc-ec2-autoscale" {
+module "srecc-ec2-backend" {
   source  = "mujahidhemani/srecc-ec2-autoscale/aws"
+  version = "1.1.0"
+  vpc_id = "${module.srecc-vpc.vpc_id}"
+  subnet_ids = "${module.srecc-vpc.all_subnet_ids}"
+  target_group_arn = "${module.srecc-frontend.target_group_arn}"
+}
+
+module "srecc-frontend" {
+  source  = "mujahidhemani/srecc-load-balancer/aws"
   version = "1.0.0"
   vpc_id = "${module.srecc-vpc.vpc_id}"
   subnet_ids = "${module.srecc-vpc.all_subnet_ids}"
-}
+  autoscaling_group_name = "${module.srecc-ec2-backend.autoscaling_group_name}"
 
+}
