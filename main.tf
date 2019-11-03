02926e903e80307e708eb9ab66831327aebaf2f4
Mujahid Hemani <9062439+mujahidhemani@users.noreply.github.com>
2019-11-03 20:21:11 +0000

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  
+
 }
 
 module "srecc-vpc" {
@@ -9,18 +9,18 @@ module "srecc-vpc" {
 
 
 module "srecc-ec2-backend" {
-  source  = "mujahidhemani/srecc-ec2-autoscale/aws"
-  version = "1.1.0"
-  vpc_id = "${module.srecc-vpc.vpc_id}"
-  subnet_ids = "${module.srecc-vpc.all_subnet_ids}"
+  source           = "mujahidhemani/srecc-ec2-autoscale/aws"
+  version          = "1.1.0"
+  vpc_id           = "${module.srecc-vpc.vpc_id}"
+  subnet_ids       = "${module.srecc-vpc.all_subnet_ids}"
   target_group_arn = "${module.srecc-frontend.target_group_arn}"
 }
 
 module "srecc-frontend" {
-  source  = "mujahidhemani/srecc-load-balancer/aws"
-  version = "1.0.0"
-  vpc_id = "${module.srecc-vpc.vpc_id}"
-  subnet_ids = "${module.srecc-vpc.all_subnet_ids}"
+  source                 = "mujahidhemani/srecc-load-balancer/aws"
+  version                = "1.0.0"
+  vpc_id                 = "${module.srecc-vpc.vpc_id}"
+  subnet_ids             = "${module.srecc-vpc.all_subnet_ids}"
   autoscaling_group_name = "${module.srecc-ec2-backend.autoscaling_group_name}"
 
 }
