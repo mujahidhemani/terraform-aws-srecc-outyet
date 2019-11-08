9a34e2f4924aa25920b43e7117f60ac69bdae134
Mujahid Hemani <9062439+mujahidhemani@users.noreply.github.com>
2019-11-08 01:03:45 -0500

    Update main.tf
    bump versions of vpc and ec2 autoscale

--- main.tf
+++ main.tf
@@ -4,15 +4,15 @@ provider "aws" {
 
 module "srecc-vpc" {
   source  = "mujahidhemani/srecc-vpc/aws"
-  version = "1.0.1"
+  version = "1.1.0"
 }
 
 
 module "srecc-ec2-backend" {
   source           = "mujahidhemani/srecc-ec2-autoscale/aws"
-  version          = "1.1.3"
+  version          = "1.2.0"
   vpc_id           = "${module.srecc-vpc.vpc_id}"
-  subnet_ids       = "${module.srecc-vpc.all_subnet_ids}"
+  subnet_ids       = "${module.srecc-vpc.backend_subnet_ids}"
   target_group_arn = "${module.srecc-frontend.target_group_arn}"
 }
 
@@ -20,7 +20,7 @@ module "srecc-frontend" {
   source                 = "mujahidhemani/srecc-load-balancer/aws"
   version                = "1.1.0"
   vpc_id                 = "${module.srecc-vpc.vpc_id}"
-  subnet_ids             = "${module.srecc-vpc.all_subnet_ids}"
+  subnet_ids             = "${module.srecc-vpc.frontend_subnet_ids}"
   autoscaling_group_name = "${module.srecc-ec2-backend.autoscaling_group_name}"
   backend_app_sg_id      = "${module.srecc-ec2-backend.backend_sg_id}"
 
