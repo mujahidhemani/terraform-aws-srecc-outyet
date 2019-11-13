0017bba542761c36b5298f788a7cce7769bb8b63
Mujahid Hemani <9062439+mujahidhemani@users.noreply.github.com>
2019-11-12 22:38:41 -0500

    Update main.tf
    add https support, and create a friendly DNS record in a cloudflare zone

--- main.tf
+++ main.tf
@@ -2,9 +2,13 @@ provider "aws" {
 
 }
 
+provider "cloudflare" {
+
+}
+
 module "srecc-vpc" {
   source  = "mujahidhemani/srecc-vpc/aws"
-  version = "1.1.0"
+  version = "1.1.1"
 }
 
 
@@ -14,14 +18,23 @@ module "srecc-ec2-backend" {
   vpc_id           = "${module.srecc-vpc.vpc_id}"
   subnet_ids       = "${module.srecc-vpc.backend_subnet_ids}"
   target_group_arn = "${module.srecc-frontend.target_group_arn}"
+  tls_cert_arn     = "${var.tls_cert_arn}"
 }
 
 module "srecc-frontend" {
   source                 = "mujahidhemani/srecc-load-balancer/aws"
-  version                = "1.1.0"
+  version                = "1.2.0"
   vpc_id                 = "${module.srecc-vpc.vpc_id}"
   subnet_ids             = "${module.srecc-vpc.frontend_subnet_ids}"
   autoscaling_group_name = "${module.srecc-ec2-backend.autoscaling_group_name}"
   backend_app_sg_id      = "${module.srecc-ec2-backend.backend_sg_id}"
+}
 
+module "srecc-cloudflare-dns" {
+  source             = "mujahidhemani/srecc-cloudflare-dns/aws"
+  version            = "1.0.0"
+  record_type        = "CNAME"
+  record_name        = var.cloudflare_dns_record_name
+  record_value       = module.srecc-frontend.lb_dns_name
+  cloudflare_zone_id = var.cloudflare_zone_id
 }
